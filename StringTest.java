ac797fb07b70bfa9a46647db6a784ae52b9982eb
Sezer <sezercetinkaya24@gmail.com>
2026-03-04 01:05:41 +0300

    Create StringTest class for string manipulation
    Add StringTest class to demonstrate string manipulation.

---
+++ StringTest.java
@@ -0,0 +1,9 @@
+public class StringTest {
+    public static void main(String[] args) {
+        String sehir = "istanbul";
+        
+        sehir.toUpperCase();  //.toUpperCase() metottur. Aldığı girişleri büyük harf yapar ve çıktı olarak verir.
+        
+        System.out.println(sehir);
+    }
+}

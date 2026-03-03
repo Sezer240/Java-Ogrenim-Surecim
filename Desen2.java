f03e856febadec15094ae1b3186794f8acf19faa
Sezer <sezercetinkaya24@gmail.com>
2026-03-04 00:50:20 +0300

    Add Desen2.java to print a checkerboard pattern

---
+++ Desen2.java
@@ -0,0 +1,18 @@
+public class Desen2 {
+    public static void main(String[] args) {
+        int N = 7;
+
+        for (int i = 0; i < N; i++) { //satır gezme 
+            for (int j = 0; j < N; j++) { //sutün gezme 
+                if (i % 2 == 0 && j % 2 == 0) { //i ve j'nin çift olduğu koordinatları 1 yap
+                    System.out.print("1 ");
+                } else if (i % 2 != 0 && j % 2 != 0) { //i ve j'nin tek olduğu koordinatları 1 yap
+                    System.out.print("1 ");
+                } else { //dışarda kalanları 0 yap
+                    System.out.print("0 ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}

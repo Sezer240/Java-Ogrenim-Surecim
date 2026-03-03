9c9c0c26e45a0186d11d2794303747df18cda481
Sezer <sezercetinkaya24@gmail.com>
2026-03-04 00:50:56 +0300

    Fix formatting of nested loops in Desen2.java

--- Desen2.java
+++ Desen2.java
@@ -2,8 +2,8 @@ public class Desen2 {
     public static void main(String[] args) {
         int N = 7;
 
-        for (int i = 0; i < N; i++) { //satır gezme 
-            for (int j = 0; j < N; j++) { //sutün gezme 
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
                 if (i % 2 == 0 && j % 2 == 0) { //i ve j'nin çift olduğu koordinatları 1 yap
                     System.out.print("1 ");
                 } else if (i % 2 != 0 && j % 2 != 0) { //i ve j'nin tek olduğu koordinatları 1 yap
